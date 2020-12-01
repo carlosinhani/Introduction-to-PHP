aa6525897993029ea210f589725335c99db482df
Carlos.Inhani <carlos.inhani@gmail.com>
2020-12-01 08:39:45 -0300

    add_servicos

--- php/index.php
+++ php/index.php
@@ -1,3 +1,7 @@
+<?php
+include "servicos/servicoMensagemSessao.php";
+?>
+<!DOCTYPE html>
 <html>
 
 <head>
@@ -10,13 +14,27 @@
 
 <body>
 
-<p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDOES</p>
+<p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</p>
 
 <form action="script.php" method="post">
+    <?php
+        $mensagemDeSucesso = obterMensagemSucesso();
+        if(!empty($mensagemDeSucesso))
+        {
+            echo $mensagemDeSucesso;
+        }
+
+        $mensagemDeErro = obterMensagemErro();
+        if(!empty($mensagemDeErro))
+        {
+            echo $mensagemDeErro;
+        }
+    ?>
     <p>Seu nome: <input type="text" name="nome" /></p>
     <p>Sua idade: <input type="text" name="idade" /></p>
     <p><input type="submit" value="Enviar dados do competidor"/></p>
 </form>
+
 </body>
 
 </html>
---
+++ php/script.php
@@ -0,0 +1,13 @@
+<?php
+
+include "servicos/servicoMensagemSessao.php";
+include "servicos/servicoValidacao.php";
+include "servicos/servicoCategoriaCompetidor.php";
+
+
+$nome = $_POST['nome'];
+$idade = $_POST['idade'];
+
+defineCategoriaCompetidor($nome, $idade);
+
+header('location: index.php');
